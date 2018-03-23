52451f812c3776f5b0b0a53e5fbc458ed335fbe4
Kara Mui <kara.mui@dartmouth.edu>
2018-03-22 21:17:33 -0500

    added express server functionality

---
+++ server.js
@@ -0,0 +1,23 @@
+// initializes Node.js packages
+var bodyParser = require("body-parser");
+var express = require("express");
+
+// initializes Express.js server and defines port
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// sets up data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// imports routes
+/*var routes = require("./controllers/CONTROLLERNAME.js");
+app.use(routes);*/
+
+// loads static files
+app.use(express.static("./public"));
+
+// starts Express.js server
+app.listen(PORT, function() {
+	console.log("This app is listening on PORT: " + PORT + ".");
+});
